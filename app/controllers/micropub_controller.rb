be03acc5b8a3d5889fe4015bb9833f1254f15639
Gorka <4787906+gorka@users.noreply.github.com>
2024-04-15 16:39:18 -0500

    Micropub 5 (#12)
    * Add deleted_at
    
    * Implement 500: Delete a post (form-encoded)
    
    * Implement 501: Delete a post (JSON)
    
    * Implement 502: Undelete a post (form-encoded)
    
    * Implement 503: Undelete a post (JSON)

--- app/controllers/micropub_controller.rb
+++ app/controllers/micropub_controller.rb
@@ -17,11 +17,67 @@ class MicropubController < ApplicationController
     }
   }
 
+  PERMITTED_ACTIONS = %w[ delete undelete ]
+
+  class InvalidAction < StandardError; end
   class InvalidMicroformat < StandardError; end
 
   def create
     if request.content_type =~ CONTENT_TYPES[:FORM_ENCODED] || request.content_type =~ CONTENT_TYPES[:MULTIPART]
-      microformat = MICROFORMAT_OBJECT_TYPES[params[:h].to_sym]
+      action = request.POST[:action]
+      microformat = params[:h]
+
+      if valid_action = PERMITTED_ACTIONS.include?(action)
+        send("form_#{action}_action") and return
+      end
+
+      if !action && microformat
+        form_create_action and return
+      end
+
+      raise InvalidMicroformat if !microformat
+      raise InvalidAction      
+    end
+
+    if request.content_type =~ CONTENT_TYPES[:JSON]
+      action = params[:micropub][:action]
+      microformat_sym = params[:type]&.first&.split("-")&.pop&.to_sym
+
+      if valid_action = PERMITTED_ACTIONS.include?(action)
+        send("json_#{action}_action") and return
+      end
+
+      if !action && microformat = MICROFORMAT_OBJECT_TYPES[microformat_sym]
+        json_create_action(microformat) and return
+      end
+
+      raise InvalidMicroformat if !microformat
+      raise InvalidAction
+    end
+  end
+
+  private
+
+    def authenticate
+      # https://tokens.indieauth.com/#verify
+
+      token = http_header_token || post_body_token
+
+      render json: {
+        "error": "unauthorized",
+        "error_description": "You must provide a Bearer token."
+      }, status: :unauthorized and return if !token
+
+      render json: {
+        "error": "bad request",
+        "error_description": "You must provide a Bearer token."
+      }, status: :bad_request and return if http_header_token && post_body_token
+
+      verify_token(token)
+    end
+
+    def form_create_action
+      microformat = MICROFORMAT_OBJECT_TYPES[params[:h]&.to_sym]
 
       raise InvalidMicroformat if !microformat
 
@@ -89,12 +145,51 @@ class MicropubController < ApplicationController
       end
     end
 
-    if request.content_type =~ CONTENT_TYPES[:JSON]
-      microformat_param = params[:type]&.first&.split("-")&.pop
-      microformat = MICROFORMAT_OBJECT_TYPES[microformat_param.to_sym]
+    def form_delete_action
+      resource = resource_from_url(params[:url])
 
-      raise InvalidMicroformat if !microformat
+      if resource.update(deleted_at: Time.now)
+        head :no_content
+      else
+        render json: {
+          "error": "bad request",
+          "error_description": "Something wen't wrong when deleting this resource."
+        }, status: :bad_request
+      end
+    end
 
+    def form_undelete_action
+      resource = resource_from_url(params[:url])
+
+      if resource.update(deleted_at: nil)
+        head :no_content
+      else
+        render json: {
+          "error": "bad request",
+          "error_description": "Something wen't wrong when undeleting this resource."
+        }, status: :bad_request
+      end
+    end
+
+    def resource_from_url(url)
+      path = URI.parse(url)&.path
+      return unless path
+
+      route = Rails.application.routes.recognize_path(path)
+
+      microformat_sym = route[:controller].singularize.to_sym
+      return unless microformat_sym
+
+      microformat = MICROFORMAT_OBJECT_TYPES[microformat_sym]
+      return unless microformat
+
+      microformat_class = microformat[:class]
+      return unless microformat_class
+
+      microformat_class.unscoped.find_by(id: route[:id])
+    end
+
+    def json_create_action(microformat)
       properties = params[:properties]
 
       microformat_object = microformat[:class].new
@@ -185,6 +280,32 @@ class MicropubController < ApplicationController
       verify_token(token)
     end
 
+    def json_delete_action
+      resource = resource_from_url(params[:url])
+
+      if resource.update(deleted_at: Time.now)
+        head :no_content
+      else
+        render json: {
+          "error": "bad request",
+          "error_description": "Something wen't wrong when deleting this resource."
+        }, status: :bad_request
+      end
+    end
+
+    def json_undelete_action
+      resource = resource_from_url(params[:url])
+
+      if resource.update(deleted_at: nil)
+        head :no_content
+      else
+        render json: {
+          "error": "bad request",
+          "error_description": "Something wen't wrong when undeleting this resource."
+        }, status: :bad_request
+      end
+    end
+
     def http_header_token
       authenticate_with_http_token { |token, _options| token }
     end
@@ -194,8 +315,6 @@ class MicropubController < ApplicationController
     end
 
     def verify_token(token)
-      
-
       # todo: use blog's token_endpoint
       token_endpoint = "https://tokens.indieauth.com/token"
 
---
+++ test/controllers/micropub_rocks/5_test.rb
@@ -0,0 +1,182 @@
+require "test_helper"
+
+class MicropubRocks5Test < ActionDispatch::IntegrationTest
+  setup do
+    # todo: mock to avoid http requests during tests.
+    @headers = {
+      "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJtZSI6Imh0dHBzOlwvXC9maW5jaC1wb3B1bGFyLWltcGFsYS5uZ3Jvay1mcmVlLmFwcFwvIiwiaXNzdWVkX2J5IjoiaHR0cHM6XC9cL3Rva2Vucy5pbmRpZWF1dGguY29tXC90b2tlbiIsImNsaWVudF9pZCI6Imh0dHBzOlwvXC9taWNyb3B1Yi5yb2Nrc1wvIiwiaXNzdWVkX2F0IjoxNjkzODU3ODk3LCJzY29wZSI6ImNyZWF0ZSB1cGRhdGUgZGVsZXRlIHVuZGVsZXRlIiwibm9uY2UiOjg4Njc4OTY5fQ.BPoiJoCYxobqK8QJHc3MeolyStkEZl5pQIw2pD9isNg"
+    }
+  end
+
+  test "500: Delete a post (form-encoded)" do
+    # Create post:
+    create_data = {
+      h: "entry",
+      content: "This post will be deleted when the test succeeds."
+    }
+
+    post micropub_path, params: create_data, headers: @headers
+
+    assert_response :created
+
+    # Verify post exists:
+    last_entry = Entry.last
+
+    get entry_url(last_entry)
+
+    assert_select ".h-entry", count: 1
+    assert_select ".e-content", text: "This post will be deleted when the test succeeds."
+
+    # Delete post:
+    delete_data = {
+      action: "delete",
+      url: entry_url(last_entry)
+    }
+
+    post micropub_path, params: delete_data, headers: @headers
+
+    assert_response :no_content
+
+    # Verify post has been deleted:
+    get entry_url(last_entry)
+
+    assert_response :not_found
+  end
+
+  test "501: Delete a post (JSON)" do
+    # Create post:
+    data = {
+      "type": ["h-entry"],
+      "properties": {
+        "content": ["This post will be deleted when the test succeeds."]
+      }
+    }
+
+    post micropub_path, params: data, as: :json, headers: @headers
+
+    assert_response :created
+
+    # Verify post exists:
+    last_entry = Entry.last
+
+    get entry_url(last_entry)
+
+    assert_select ".h-entry", count: 1
+    assert_select ".e-content", text: "This post will be deleted when the test succeeds."
+
+    # Delete post:
+    delete_data = {
+      "action": "delete",
+      "url": entry_url(last_entry)
+    }
+
+    post micropub_path, params: delete_data, as: :json, headers: @headers
+
+    assert_response :no_content
+
+    # Verify post has been deleted:
+    get entry_url(last_entry)
+
+    assert_response :not_found
+  end
+
+  test "502: Undelete a post (form-encoded)" do
+    # Create post:
+    create_data = {
+      h: "entry",
+      content: "This post will be deleted, and should be restored after undeleting it."
+    }
+
+    post micropub_path, params: create_data, headers: @headers
+
+    assert_response :created
+
+    # Verify post exists:
+    last_entry = Entry.last
+
+    get entry_url(last_entry)
+
+    assert_select ".h-entry", count: 1
+    assert_select ".e-content", text: "This post will be deleted, and should be restored after undeleting it."
+
+    # Delete post:
+    delete_data = {
+      action: "delete",
+      url: entry_url(last_entry)
+    }
+
+    post micropub_path, params: delete_data, headers: @headers
+
+    assert_response :no_content
+
+    # Verify post has been deleted:
+    get entry_url(last_entry)
+
+    assert_response :not_found
+
+    # Undelete post:
+    undelete_data = {
+      action: "undelete",
+      url: entry_url(last_entry)
+    }
+
+    post micropub_path, params: undelete_data, headers: @headers
+
+    # Verify post has been undeleted:
+    get entry_url(last_entry)
+
+    assert_select ".h-entry", count: 1
+    assert_select ".e-content", text: "This post will be deleted, and should be restored after undeleting it."
+  end
+
+  test "503: Undelete a post (JSON)" do
+        # Create post:
+        data = {
+          "type": ["h-entry"],
+          "properties": {
+            "content": ["This post will be deleted, and should be restored after undeleting it."]
+          }
+        }
+    
+        post micropub_path, params: data, as: :json, headers: @headers
+    
+        assert_response :created
+    
+        # Verify post exists:
+        last_entry = Entry.last
+    
+        get entry_url(last_entry)
+    
+        assert_select ".h-entry", count: 1
+        assert_select ".e-content", text: "This post will be deleted, and should be restored after undeleting it."
+    
+        # Delete post:
+        delete_data = {
+          "action": "delete",
+          "url": entry_url(last_entry)
+        }
+    
+        post micropub_path, params: delete_data, as: :json, headers: @headers
+    
+        assert_response :no_content
+    
+        # Verify post has been deleted:
+        get entry_url(last_entry)
+    
+        assert_response :not_found
+
+        # Undelete post:
+        undelete_data = {
+          action: "undelete",
+          url: entry_url(last_entry)
+        }
+
+        post micropub_path, params: undelete_data, as: :json, headers: @headers
+
+        # Verify post has been undeleted:
+        get entry_url(last_entry)
+
+        assert_select ".h-entry", count: 1
+        assert_select ".e-content", text: "This post will be deleted, and should be restored after undeleting it."
+  end
+end
