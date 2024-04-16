957e74ea7774e3286077261756f2792c755ce13f
Gorka <gorka@hey.com>
2024-04-16 12:18:13 -0500

    Remove CDS strategy from lib

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,36 @@
-require_relative "../../lib/omniauth/strategies/cds"
+module OmniAuth
+  module Strategies
+    class Cds < OmniAuth::Strategies::OAuth2
+      option :name, "cds"
+
+      option :client_options, {
+        site: "https://cajondesaastre.com",
+        authorize_path: "/oauth/authorize"
+      }
+
+      uid do
+        raw_info["id"]
+      end
+
+      info do
+        {
+          name: raw_info["name"],
+          email: raw_info["email"],
+        }
+      end
+
+      extra do
+        {
+          raw_info: raw_info
+        }
+      end
+
+      def raw_info
+        @raw_info ||= access_token.get("/api/v1/me").parsed
+      end
+    end
+  end
+end
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer if Rails.env.development? || Rails.env.test?
