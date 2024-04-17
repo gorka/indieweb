d2a0d1177cf9799ef24ed0e4e788bb56bb4f816a
Gorka <gorka@hey.com>
2024-04-17 13:04:16 -0500

    Implement 603: Source Query (Specific Properties)

--- test/controllers/micropub_rocks/6_test.rb
+++ test/controllers/micropub_rocks/6_test.rb
@@ -35,9 +35,60 @@ class MicropubRocks6Test < ActionDispatch::IntegrationTest
 
     assert_response :created
 
+    # Query full entry
     get micropub_url(subdomain: blog.subdomain, q: "source", url: entry_url(Entry.last))
     
     assert_response :ok
     assert_equal data.to_json, response.body
+  end
+
+  test "603: Source Query (Specific Properties)" do
+    # Create entry
+    blog = blogs(:valid)
+    data = {
+      type: ["h-entry"],
+      properties: {
+        content: ["Test of querying the endpoint for the source content"],
+        category: ["micropub", "test"]
+      }
+    }
+
+    post micropub_url(subdomain: blog.subdomain), params: data, as: :json, headers: @headers
+
+    assert_response :created
+
+    # Query entry properties
+    get micropub_url(subdomain: blog.subdomain, q: "source", properties: ["content", "category"], url: entry_url(Entry.last))
+
+    assert_response :ok
+    assert_equal data.to_json, response.body
+  end
+
+  test "603: Source Query (Specific Properties) (extra)" do
+    # Create entry
+    blog = blogs(:valid)
+    data = {
+      type: ["h-entry"],
+      properties: {
+        content: ["Test of querying the endpoint for the source content"],
+        category: ["micropub", "test"]
+      }
+    }
+
+    post micropub_url(subdomain: blog.subdomain), params: data, as: :json, headers: @headers
+
+    assert_response :created
+
+    # Query entry properties
+    response_data = {
+      type: ["h-entry"],
+      properties: {
+        content: ["Test of querying the endpoint for the source content"]
+      }
+    }
+    get micropub_url(subdomain: blog.subdomain, q: "source", properties: ["content"], url: entry_url(Entry.last))
+
+    assert_response :ok
+    assert_equal response_data.to_json, response.body
   end
 end
