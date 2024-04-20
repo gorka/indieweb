10c067958eb4e4c7964ee289654d4bc9369657ad
Gorka <gorka@hey.com>
2024-04-20 15:58:14 -0500

    Log domain on public application

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -3,9 +3,15 @@ class PublicController < ActionController::Base
 
   before_action :set_blog
 
+  before_action :log_domain
+
   private
 
     def set_blog
       @blog = Blog.find_by(subdomain: request.subdomain)
+    end
+
+    def log_domain
+      logger.info("-> domain: #{request.host} & subdomain: #{request.subdomain}")
     end
 end
