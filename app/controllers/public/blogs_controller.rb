947cbe30b6c6c737f900f7a8c5ab41a787266c75
Gorka <gorka@hey.com>
2024-04-24 22:51:46 -0500

    Hide drafts in list for no owners

--- app/controllers/public/blogs_controller.rb
+++ app/controllers/public/blogs_controller.rb
@@ -1,4 +1,10 @@
 class Public::BlogsController < PublicController
   def show
+    # todo: simplify with pundit.
+    if current_user == @blog.user
+      @entries = @blog.entries
+    else
+      @entries = @blog.entries.where(status: "published")
+    end
   end
 end
