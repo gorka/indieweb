3e8c30312cf5fe531fba25762af21d8782327f7c
Gorka <gorka@hey.com>
2024-05-15 08:18:52 -0500

    Fix some n+1

--- app/controllers/public/blogs_controller.rb
+++ app/controllers/public/blogs_controller.rb
@@ -7,6 +7,8 @@ class Public::BlogsController < PublicController
       entries = Current.blog.entries.where(status: "published")
     end
 
+    entries = entries.includes(:categories, photos_with_alt: { photo_attachment: :blob }).strict_loading
+
     @pagy, @entries = pagy(entries)
   end
 end
